6507e5e982da05dc9d84f47e9e6d27a8831665c2
deepak-dhull-01 <deepak.dhull@prgx.com>
2022-02-12 22:02:20 +0530

    review service refactoring

--- src/main/java/com/amcart/service/productreview/ProductReviewApplication.java
+++ src/main/java/com/amcart/service/productreview/ProductReviewApplication.java
@@ -3,7 +3,7 @@ package com.amcart.service.productreview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.amcart.service.common", "com.amcart.service.productreview"})
 public class ProductReviewApplication {
 
 	public static void main(String[] args) {

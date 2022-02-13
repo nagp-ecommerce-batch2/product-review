81005b0c50c034ea15f7c580ccc9f557554c4836
deepak-dhull-01 <deepak.dhull@prgx.com>
2022-02-13 13:57:17 +0530

    lombok removed

--- src/main/java/com/amcart/service/productreview/models/ReviewDTO.java
+++ src/main/java/com/amcart/service/productreview/models/ReviewDTO.java
@@ -2,15 +2,7 @@ package com.amcart.service.productreview.models;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ReviewDTO {
 	
 	private String reviewId;
@@ -21,6 +13,76 @@ public class ReviewDTO {
     private String reviewDate;
     private String title;
     
+    
+    public ReviewDTO() {
+    }
+
+    public ReviewDTO(String reviewId, String productId, String userId, String comment, double rating, String reviewDate, String title) {
+        this.reviewId = reviewId;
+        this.productId = productId;
+        this.userId = userId;
+        this.comment = comment;
+        this.rating = rating;
+        this.reviewDate = reviewDate;
+        this.title = title;
+    }
+
+    public String getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String getReviewDate() {
+		return reviewDate;
+	}
+
+	public void setReviewDate(String reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 
     @Override
     public String toString() {
